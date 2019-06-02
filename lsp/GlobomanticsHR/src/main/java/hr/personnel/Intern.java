95099682581530cdf462d67f3963cc563c192cca
Dan Geabunea <dangeabunea@hotmail.com>
2019-06-02 13:37:24 +0300

    begun work on lsp;

---
+++ lsp/GlobomanticsHR/src/main/java/hr/personnel/Intern.java
@@ -0,0 +1,8 @@
+package hr.personnel;
+
+public class Intern extends Employee {
+    public Intern(String fullName, int monthlyIncome, int nbHours) {
+        super(fullName, monthlyIncome);
+        setNbHoursPerWeek(nbHours);
+    }
+}
